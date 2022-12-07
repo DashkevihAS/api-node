26803850be2282378249295e7de276030dc5cf09
DashkevichAS <antonsnov0604@gmail.com>
2022-12-07 16:41:38 +0300

    post & redirect

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -20,6 +21,12 @@ app.get('/user/:username', (req, res) => {
   };
 
   res.render('user', data);
+});
+
+app.post('/check-user', (req, res) => {
+  let username = req.body.username;
+  if (username === '') return res.redirect('/');
+  else return res.redirect(`/user/${req.body.username}`);
 });
 
 const PORT = 3000;
